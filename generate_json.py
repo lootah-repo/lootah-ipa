be242f8b29799f3564fa10d0d259946c5024a76f
lootah-repo <149418995+lootah-repo@users.noreply.github.com>
2023-11-01 03:48:08 +0400

    Update generate_json.py

--- generate_json.py
+++ generate_json.py
@@ -4,6 +4,8 @@ import argparse
 import pandas as pd
 from get_bundle_id import get_single_bundle_id
 import os
+import shutil
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -11,7 +13,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
     token = args.token
 
-    with open("apps.json", "r") as f:
+    out_file = "apps.json"
+    clone_file = "index.html"
+
+    with open(out_file, "r") as f:
         data = json.load(f)
 
     if os.path.exists("bundleId.csv"):
@@ -23,22 +28,28 @@ if __name__ == "__main__":
     data["apps"] = []
 
     g = Github(token)
-    repo = g.get_repo("lootah-repo/lootah-ipa")
+    repo_name = "lootah-repo/lootah-ipa"
+    repo = g.get_repo(repo_name)
     releases = repo.get_releases()
 
     for release in releases:
         print(release.title)
 
         for asset in release.get_assets():
-            if (asset.name[-3:] != "ipa"):
+            if not asset.name.endswith(".ipa"):
                 continue
             name = asset.name[:-4]
             date = asset.created_at.strftime("%Y-%m-%d")
+            full_date = asset.created_at.strftime("%Y%m%d%H%M%S")
             try:
-                app_name, version = name.split("-", 1)
+                app_name, version, tweaks = name.split("_", 2)
+                tweaks, _ = tweaks.split("@", 1)
+                if tweaks:
+                    tweaks = "Injected with " + tweaks[:-1].replace("_", " ")
             except:
                 app_name = name
-                version = "1.0"
+                version = "Unknown"
+                tweaks = None
 
             if app_name in df.name.values:
                 bundle_id = str(df[df.name == app_name].bundleId.values[0])
@@ -50,18 +61,26 @@ if __name__ == "__main__":
             data["apps"].append(
                 {
                     "name": app_name,
+                    "realBundleID": bundle_id,
+                    "bundleID": bundle_id,
                     "bundleIdentifier": bundle_id,
                     "version": version,
                     "versionDate": date,
+                    "fullDate": full_date,
                     "size": asset.size,
+                    "down": asset.browser_download_url,
                     "downloadURL": asset.browser_download_url,
                     "developerName": "",
-                    "localizedDescription": "",
-                    "iconURL": f"https://raw.githubusercontent.com/lootah-repo/lootah-ipa/main/icons/{bundle_id}.png"
+                    "localizedDescription": tweaks,
+                    "icon": f"https://raw.githubusercontent.com/{repo_name}/main/icons/{bundle_id}.png",
+                    "iconURL": f"https://raw.githubusercontent.com/{repo_name}/main/icons/{bundle_id}.png"
                 }
             )
+            data["apps"].sort(key=lambda x: x["fullDate"], reverse=True)
 
     df.to_csv("bundleId.csv", index=False)
 
-    with open('apps.json', 'w') as json_file:
+    with open(out_file, 'w') as json_file:
         json.dump(data, json_file, indent=4)
+
+    shutil.copyfile(out_file, clone_file)
