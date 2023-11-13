0aeaefbe85b170ef8189a8ef6a17e7eafc75952a
lootah-repo <149418995+lootah-repo@users.noreply.github.com>
2023-11-13 10:04:40 +0400

    Update get_bundle_id.py

--- get_bundle_id.py
+++ get_bundle_id.py
@@ -1,61 +1,48 @@
 import requests
 import zipfile
 import plistlib
-import github
+from github import Github
 import pandas as pd
 import shutil
 import os
 
-
-def get_single_bundle_id(url, name="temp.ipa"):
-    reponse = requests.get(url)
-    open(name, 'wb').write(reponse.content)
-
-    icon_folder = "icons/"
-    if not os.path.exists(icon_folder):
-        os.mkdir(icon_folder)
+def download_and_extract(url, name="temp.ipa"):
+    response = requests.get(url)
+    open(name, 'wb').write(response.content)
 
     with zipfile.ZipFile(name, mode="r") as archive:
         for file_name in archive.namelist():
             if file_name.endswith(".app/Info.plist"):
                 info_file = file_name
-                folder_path = os.path.dirname(info_file)
 
         with archive.open(info_file) as fp:
-            pl = plistlib.load(fp)
-            icon_path = ""
-            bundleId = pl["CFBundleIdentifier"]
-            if "CFBundleIconFiles" in pl.keys():
-                try:
-                    icon_path = os.path.join(
-                        folder_path, pl["CFBundleIconFiles"][0])
-                except:
-                    # index [0] out-of-range: empty icon list
-                    return bundleId
-            if "CFBundleIcons" in pl.keys():
-                try:
-                    icon_prefix = pl["CFBundleIcons"]["CFBundlePrimaryIcon"]["CFBundleIconFiles"][0]
-                except:
-                    icon_prefix = pl["CFBundleIcons"]["CFBundlePrimaryIcon"]["CFBundleIconName"]
-                for file_name in archive.namelist():
-                    if icon_prefix in file_name:
-                        icon_path = file_name
-            if icon_path:
-                try:
-                    with archive.open(icon_path) as origin, open(icon_folder + bundleId + ".png", "wb") as dst:
-                        shutil.copyfileobj(origin, dst)
-                except:
-                    pass
-            else:  # no icon info
-                pass
+            return plistlib.load(fp)
 
-            return bundleId
+def get_bundle_id_and_icon(url, name="temp.ipa", icon_folder="icons/"):
+    pl = download_and_extract(url, name)
 
-    return "com.example.app"
+    bundle_id = pl.get("CFBundleIdentifier", "com.example.app")
+    icon_path = ""
 
+    if "CFBundleIconFiles" in pl:
+        icon_path = os.path.join(os.path.dirname(info_file), pl["CFBundleIconFiles"][0])
+    elif "CFBundleIcons" in pl:
+        icon_prefix = pl["CFBundleIcons"]["CFBundlePrimaryIcon"]["CFBundleIconFiles"][0]
+        for file_name in archive.namelist():
+            if icon_prefix in file_name:
+                icon_path = file_name
+
+    if icon_path:
+        try:
+            with archive.open(icon_path) as origin, open(icon_folder + bundle_id + ".png", "wb") as dst:
+                shutil.copyfileobj(origin, dst)
+        except Exception as e:
+            print(f"Error copying icon: {e}")
+
+    return bundle_id
 
 def generate_bundle_id_csv(token, repo_name="lootah-repo/lootah-ipa"):
-    g = github.Github(token)
+    g = Github(token)
     repo = g.get_repo(repo_name)
     releases = repo.get_releases()
 
@@ -64,25 +51,30 @@ def generate_bundle_id_csv(token, repo_name="lootah-repo/lootah-ipa"):
     for release in releases:
         print(release.title)
         for asset in release.get_assets():
-            if (asset.name[-3:] != "ipa"):
+            if not asset.name.endswith("ipa"):
                 continue
             name = asset.name[:-4]
             print(asset.name)
 
             try:
                 app_name = name.split("-", 1)[0]
-            except:
+            except Exception as e:
+                print(f"Error parsing app name: {e}")
                 app_name = name
 
-            if app_name in df.name.values:
+            bundle_id = get_bundle_id_and_icon(asset.browser_download_url)
+
+            # Check if the bundle identifier already exists in the DataFrame
+            if not df[df["bundleId"] == bundle_id].empty:
                 continue
+
             df = pd.concat(
                 [
                     df,
                     pd.DataFrame(
                         {
                             "name": [app_name],
-                            "bundleId": get_single_bundle_id(asset.browser_download_url)
+                            "bundleId": [bundle_id]
                         }
                     )
                 ],
@@ -90,7 +82,6 @@ def generate_bundle_id_csv(token, repo_name="lootah-repo/lootah-ipa"):
             )
 
     df.to_csv("bundleId.csv", index=False)
-
 
 if __name__ == "__main__":
     generate_bundle_id_csv(None)
